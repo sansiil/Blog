ef560616008dd431d0282e684b3b93821dc191ef
sansiil <sahanshiludash@gmail.com>
2023-01-10 22:05:17 +0545

    js touched,css left

---
+++ blog.js
@@ -0,0 +1,10 @@
+ function cook(){
+    const cookie=document.getElementById('pp');
+    if(cookie.style.display==="none"){
+        cookie.style.display="block";
+        alert(1);
+    }
+    else{
+        cookie.style.display="none";
+    }
+ };
